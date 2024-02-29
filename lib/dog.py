2e08c9e4f5bafae424d6f1211a9f2ca142658b10
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-02-29 22:01:05 +0300

    Assignment done

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,14 @@
 
 class Dog:
     # Class body goes here
-
-    #Instance method definition
+    def bark(self):
+        print("Woof!")
+     
+    def sit(self):
+        print("The dog is sitting.")
     pass
+#Instance method definition
+fido = Dog()
+#To call bark and sit
+fido.bark()
+fido.sit()
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,13 @@
 
 class Person:
     # Class body goes here
-
+    def talk(self):
+        print("Hello World!")
     #Instance method definition
+    def walk(self):
+        print("The person is walking.")
     pass
+fido = Person()
+#To call talk and walk
+fido.talk()
+fido.walk()
